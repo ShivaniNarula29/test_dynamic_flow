1f83097985710d41c56ebacffefd35a4f3f0b387
ShivaniNarula29 <shivaninarula9211@gmail.com>
2025-03-12 00:51:41 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,11 +2,6 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "vpc_id" {}
-
-variable "public_subnet_id" {}
-variable "private_subnet_id" {}
-
 variable "key_name" {}
 variable "instance_type" {
   default = "t3.micro"
